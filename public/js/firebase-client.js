0a570915a35667ac3a4f62e04e3f9408fd0903a3
VICTOR MUBANGIZI <mubangizivic@gmail.com>
2026-08-10 10:44:33 +0300

    Connect website onboarding to School Pulse Firebase project

---
+++ public/js/firebase-client.js
@@ -0,0 +1,18 @@
+import { initializeApp, getApps } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCvVUyX1_3tuxHlhS3rdCruV08_FISvIG8',
+  appId: '1:925585407650:web:73c0b063dc0398cf9461fb',
+  messagingSenderId: '925585407650',
+  projectId: 'school-pulse-3d95b',
+  authDomain: 'school-pulse-3d95b.firebaseapp.com',
+  storageBucket: 'school-pulse-3d95b.firebasestorage.app',
+};
+
+export const firebaseApp = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+auth.useDeviceLanguage();
+
+export const functionsBaseUrl = 'https://europe-west1-school-pulse-3d95b.cloudfunctions.net';
+export const appUrl = 'https://app.schoolpulse.victorbee.com';
